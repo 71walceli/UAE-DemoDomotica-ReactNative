d9218187129b377fcabecc1aa5e676734113b108
Walter Celi <antonini2903@hotmail.com>
2023-06-21 11:39:00 -0500

    iMPLEMENTED BOTTOM NAV

---
+++ App/About.js
@@ -0,0 +1,7 @@
+import { View, Text } from "react-native"
+
+export const About = () => <>
+  <View style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "100%" }}>
+    <Text>Acerca de...</Text>
+  </View>
+</>
