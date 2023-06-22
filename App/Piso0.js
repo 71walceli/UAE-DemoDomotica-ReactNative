58ffa55aa5ef27989d41e63cbbdf83bfb9b7e6ce
Walter Celi <antonini2903@hotmail.com>
2023-06-22 10:11:21 -0500

    Fixed style issues preventing screen from irking
    - Ordered CSS props

--- App/Piso0.js
+++ App/Piso0.js
@@ -2,53 +2,63 @@ import { StyleSheet, Text, View } from 'react-native';
 import { styles } from './styles';
 import React from 'react';
 import { AscensorSwitch, LightbulbSwitch } from './Switch';
-import { AppContext } from "./App.js"
 
-export const Piso0 = _ => {
-  const {controls, handleControlChange} = React.useContext(AppContext)
-  
+
+export const Piso0 = () => {
   return <>
     <View style={styles.Map.container}>
       <View style={styles.Table}>
         <View style={styles.Table.row}>
-          <View style={{ ...styles.Table.cell, borderRight: 0, flex: "40%" }}>
+          <View style={StyleSheet.compose( styles.Table.cell, 
+              { borderRight: 0, flexGrow: 1, flexShrink: 1, flexBasis: "40%" }
+            )}>
             <View style={styles.Table}>
-              <View style={StyleSheet.compose(styles.Table.row, { flex: "30%" })}>
+              <View style={StyleSheet.compose(styles.Table.row, 
+                  { flexGrow: 1, flexShrink: 1, flexBasis: "30%" })
+                }>
                 <View style={styles.Table.cell}>
                   <Text>Baño</Text>
                   <LightbulbSwitch controlId="banio_p0" />
                 </View>
               </View>
-              <View style={StyleSheet.compose(styles.Table.row, { flex: "50%" })}>
+              <View style={StyleSheet.compose(styles.Table.row, 
+                  { flexGrow: 1, flexShrink: 1, flexBasis: "50%" })
+                }>
                 <View style={styles.Table.cell}>
                   <Text>Comedor</Text>
                   <LightbulbSwitch controlId="comedor_p0" />
                 </View>
               </View>
-              <View style={{...styles.Table.row, ...styles.Map.alleyHorizontal}}>
-                {/* <View style={StyleSheet.compose(styles.Table.cell, {border: 0})} /> */}
-              </View>
-              <View style={StyleSheet.compose(styles.Table.row, { flex: "10%" })}>
-                <View style={{...styles.Table.cell}}>
+              <View style={StyleSheet.compose( styles.Table.row, styles.Map.alleyHorizontal )} />
+              <View style={StyleSheet.compose(styles.Table.row, 
+                  { flexGrow: 1, flexShrink: 1, flexBasis: "10%" })
+                }>
+                <View style={styles.Table.cell}>
                   <Text>Ascensor</Text>
                   <AscensorSwitch controlId="ascensor" />
                 </View>
               </View>
             </View>
           </View>
-          <View style={{ ...styles.Table.cell, ...styles.Map.alleyVertical }}></View>
-          <View style={{ ...styles.Table.cell, borderLeft: 0, flex: "40%" }}>
+          <View style={StyleSheet.compose( styles.Table.cell, styles.Map.alleyVertical )} />
+          <View style={StyleSheet.compose( styles.Table.cell, 
+            { borderLeft: 0, flexGrow: 1, flexShrink: 1, flexBasis: "40%" }
+          )}>
             <View style={styles.Table}>
-              <View style={StyleSheet.compose(styles.Table.row, { flex: "45%" })}>
+              <View style={StyleSheet.compose(styles.Table.row, 
+                  { flexGrow: 1, flexShrink: 1, flexBasis: "45%" }
+              )}>
                 <View style={styles.Table.cell}>
                   <Text>Cocina</Text>
                   <LightbulbSwitch controlId="cocina" />
                 </View>
               </View>
-              <View style={{...styles.Table.row, ...styles.Map.alleyHorizontal}}>
+              <View style={StyleSheet.compose( styles.Table.row, styles.Map.alleyHorizontal )}>
               </View>
-              <View style={StyleSheet.compose(styles.Table.row, { flex: "45%" })}>
-                <View style={{...styles.Table.cell}}>
+              <View style={StyleSheet.compose(styles.Table.row, 
+                { flexGrow: 1, flexShrink: 1, flexBasis: "45%" }
+              )}>
+                <View style={styles.Table.cell}>
                   <Text>Sala</Text>
                   <LightbulbSwitch controlId="sala_p0" />
                 </View>
