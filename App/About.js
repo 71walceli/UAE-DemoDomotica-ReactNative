a0ddb607d1266cf1196d0192c7ab14e7c689627e
Walter Celi <antonini2903@hotmail.com>
2023-06-21 23:00:21 -0500

    Fixed styles which wouldn't work on Android
    Also:
    - Changed default values for controls' state
    - Fixed bulb icons not properly displayed

--- App/About.js
+++ App/About.js
@@ -1,7 +1,10 @@
-import { View, Text } from "react-native"
+import { View, Text, StyleSheet } from "react-native"
+import React from "react"
 
 export const About = () => <>
-  <View style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "100%" }}>
+  <View style={style}>
     <Text>Acerca de...</Text>
   </View>
 </>
+
+const style = StyleSheet.compose({ display: "flex", alignItems: "center", justifyContent: "center", height: "100%" })
--- App/Piso1.js
+++ App/Piso1.js
@@ -1,14 +1,20 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { styles } from './styles';
 import { AscensorSwitch, LightbulbSwitch } from './Switch';
+import React from 'react';
 
-export const Piso1 = _ => <>
+
+export const Piso1 = () => <>
   <View style={styles.Map.container}>
     <View style={styles.Table}>
       <View style={styles.Table.row}>
-        <View style={{ ...styles.Table.cell, borderRight: 0, flex: "40%" }}>
+        <View style={StyleSheet.compose( 
+          styles.Table.cell, { borderRight: 0, flexGrow: 1, flexShrink: 1, flexBasis: "40%", }
+        )}>
           <View style={styles.Table}>
-            <View style={StyleSheet.compose(styles.Table.row, { flex: "80%" })}>
+            <View style={StyleSheet.compose(
+                styles.Table.row, { flexGrow: 1, flexShrink: 1, flexBasis: "80%" }
+              )}>
               <View style={styles.Table.cell}>
                 <Text>Balcón</Text>
                 <LightbulbSwitch controlId="balcon" />
@@ -16,7 +22,7 @@ export const Piso1 = _ => <>
             </View>
             <View style={{...styles.Table.row, ...styles.Map.alleyHorizontal}}>
             </View>
-            <View style={StyleSheet.compose(styles.Table.row, { flex: "10%" })}>
+            <View style={StyleSheet.compose(styles.Table.row, { flexGrow: 1, flexShrink: 1, flexBasis: "10%" })}>
               <View style={styles.Table.cell}>
                 <Text>Ascensor</Text>
                 <AscensorSwitch controlId="ascensor" />
@@ -24,18 +30,24 @@ export const Piso1 = _ => <>
             </View>
           </View>
         </View>
-        <View style={{ ...styles.Table.cell, ...styles.Map.alleyVertical }}></View>
-        <View style={{ ...styles.Table.cell, borderLeft: 0, flex: "40%", }}>
+        <View style={StyleSheet.compose( styles.Table.cell, styles.Map.alleyVertical )}></View>
+        <View style={StyleSheet.compose( 
+          styles.Table.cell, { borderLeft: 0, flexGrow: 1, flexShrink: 1, flexBasis: "40%", }
+        )}>
           <View style={styles.Table}>
             {/* TODO Simplificar código... */}
-            <View style={StyleSheet.compose(styles.Table.row, { flex: "40%" })}>
+            <View style={StyleSheet.compose(
+                styles.Table.row, { flexGrow: 1, flexShrink: 1, flexBasis: "40%" }
+              )}>
               <View style={styles.Table.cell}>
                 <View style={styles.Table}>
-                  <View style={{...styles.Table.row, flex: "25%"}}>
-                    <View style={{...styles.Table.cell, border: 0}}>
+                  <View style={StyleSheet.compose(
+                      styles.Table.row, { flexGrow: 1, flexShrink: 1, flexBasis: "25%" }
+                    )}>
+                    <View style={StyleSheet.compose(styles.Table.cell, { border: 0 })}>
                       <View style={styles.Table}>
                         <View style={styles.Table.row}>
-                          <View style={{...styles.Table.cell, border: 0, width: "50%"}} />
+                          <View style={StyleSheet.compose(styles.Table.cell, { border: 0, width: "50%" })} />
                           <View style={styles.Table.cell}>
                             <Text>Baño</Text>
                             <LightbulbSwitch controlId="banio_p1_2" />
@@ -44,8 +56,10 @@ export const Piso1 = _ => <>
                       </View>
                     </View>
                   </View>
-                  <View style={{...styles.Table.row, flex: "75%"}}>
-                    <View style={{...styles.Table.cell, border: 0}}>
+                  <View style={StyleSheet.compose(
+                    styles.Table.row, { flexGrow: 1, flexShrink: 1, flexBasis: "75%" })}
+                  >
+                    <View style={StyleSheet.compose(styles.Table.cell, { border: 0 })}>
                       <Text>Habitación Invitados</Text>
                       <LightbulbSwitch controlId="habitacion_invitados" />
                     </View>
@@ -53,19 +67,25 @@ export const Piso1 = _ => <>
                 </View>
               </View>
             </View>
-            <View style={StyleSheet.compose(styles.Table.row, { flex: "10%" })}>
+            <View style={StyleSheet.compose(
+              styles.Table.row, { flexGrow: 1, flexShrink: 1, flexBasis: "10%" })}
+            >
               <View style={styles.Table.cell}>
                 <Text>Baño</Text>
                 <LightbulbSwitch controlId="banio_p1" />
               </View>
             </View>
-            <View style={StyleSheet.compose(styles.Table.row, { flex: "5%" })}>
+            <View style={StyleSheet.compose(
+              styles.Table.row, { flexGrow: 1, flexShrink: 1, flexBasis: "5%" })}
+            >
               <View style={styles.Table.cell}>
                 <Text>Closet</Text>
                 <LightbulbSwitch controlId="closet" />
               </View>
             </View>
-            <View style={StyleSheet.compose(styles.Table.row, { flex: "35%" })}>
+            <View style={StyleSheet.compose(
+              styles.Table.row, { flexGrow: 1, flexShrink: 1, flexBasis: "35%" })}
+            >
               <View style={styles.Table.cell}>
                 <Text>Habitación Principal</Text>
                 <LightbulbSwitch controlId="habitacion_principal" />
