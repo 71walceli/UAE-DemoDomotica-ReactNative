92179ac42dc454a7d0958b7f759adbe2231386f5
Walter Celi <antonini2903@hotmail.com>
2023-07-05 05:16:10 -0500

    Added Assets

--- App/Assets/index.js
+++ App/Assets/index.js
@@ -1,4 +1,11 @@
 import lightBulbOn from "./lightBulbOn.png"
 import lightBulbOff from "./lightBulbOff.png"
+import Muebles from "./Muebles.png"
+import Comedor from "./Comedor.png"
 
-export { lightBulbOff, lightBulbOn }
+export { 
+  lightBulbOff, 
+  lightBulbOn, 
+  Muebles, 
+  Comedor, 
+}
--- App/Piso0.js
+++ App/Piso0.js
@@ -1,13 +1,43 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 import { styles } from './styles';
 import React from 'react';
 import { AscensorSwitch, LightbulbSwitch, PuertaSwitch } from './Switch';
+import { Comedor, Muebles } from './Assets';
 
 
 export const Piso0 = () => {
+  const overlayStyle = StyleSheet.compose({ position: "absolute", zIndex: -1000, top: 0, left: 0, width: "100%", height: "100%", })
+  const absolutePositionFactory = ({width, height, top, bottom, left, right}) => {
+    width = width === 0 && "0%" || width 
+    height = height === 0 && "0%" || height 
+    top = top === 0 && "0%" || top 
+    bottom = bottom === 0 && "0%" || bottom 
+    left = left === 0 && "0%" || left 
+    right = right === 0 && "0%" || right
+    if ((left && right) || (top && bottom)) {
+      throw new TypeError("Opposite offsets can't be set at the same time.")
+    }
+    if ( !((top && left) || (top && right) || (bottom && left) || (bottom && right) )) {
+      throw new TypeError("Must specify two non-opposite directions.")
+    }
+    return StyleSheet.compose({ 
+      position: "absolute",
+      width: width || "10%",
+      height: height || "10%",
+      top,
+      bottom,
+      left,
+      right,
+     })
+  }
+
   return <>
     <View style={styles.Map.container}>
       <View style={styles.Table}>
+        <View style={overlayStyle}>
+          <Image source={Muebles} style={absolutePositionFactory({ top: "73%", right: "1%" })} />
+          <Image source={Comedor} style={absolutePositionFactory({ top: "50%", left: "20%" })} />
+        </View>
         <View style={styles.Table.row}>
           <View style={StyleSheet.compose( styles.Table.cell, 
             { borderRightWidth: 0, flexGrow: 1, flexShrink: 1, flexBasis: "40%" }
